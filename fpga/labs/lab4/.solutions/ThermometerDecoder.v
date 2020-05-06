e3600685367cbe4f5cef611ea595e6deb788f05a
Luca Pacher <pacher@to.infn.it>
2020-05-06 17:10:10 +0200

    Fixed thermometer decoder

--- fpga/labs/lab4/.solutions/ThermometerDecoder.v
+++ fpga/labs/lab4/.solutions/ThermometerDecoder.v
@@ -20,7 +20,7 @@
 module ThermometerDecoder (
 
    input  wire [4:0]  Bin,      //  5-bit base-2 binary input code
-   output reg  [31:0] Bout      // 32-bit one-hot output code
+   output reg  [31:0] Bout      // 32-bit thermometer output code
 
    ) ;
 
@@ -31,7 +31,7 @@ module ThermometerDecoder (
 
       for(i=0; i < 32; i=i+1) begin
 
-         Bout[i] = (Bin[4:0] <= i) ;      // this is equivalent to (Bin[4:0] <= i) ? 1'b1 : 1'b0 ;
+         Bout[i] = (Bin[4:0] >= i) ;      // this is equivalent to (Bin[4:0] >= i) ? 1'b1 : 1'b0 ;
 
       end  // for
    end  // always
