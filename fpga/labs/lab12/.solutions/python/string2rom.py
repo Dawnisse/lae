501f88a4d2fad1771eebccb3a3d0733afefd3411
Luca Pacher <pacher@to.infn.it>
2020-05-24 21:18:15 +0200

    ENH: generate full initial block for cut-and-paste

--- fpga/labs/lab12/.solutions/python/string2rom.py
+++ fpga/labs/lab12/.solutions/python/string2rom.py
@@ -12,12 +12,19 @@ f = open("ROM.hex", "w")
 
 k = 0
 
+print("// Verilog code starts here")
+print("initial begin")
+
 for c in message :
 	k = k + 1
 	hexCode = hex(ord(c))[2:]
 	f.write("%s\n" % hexCode)
-	print("mem[%2d] = 8'h%s" % (k,hexCode))
+	print("   mem[%2d] = 8'h%s ;" % (k,hexCode))
 
-print("\n\nROM depth must be at least %d\n\n" % (k))
+print("end")
+print("// Verilog code ends here")
+
+
+print("\n\nROM depth must be at least %d-slots deep.\n\n" % (k))
 
 f.close()
